f6561ac11c091a5e7e6eb282e38a9fe12000a112
JWard2041 <josh.wrd2@outlook.com>
2024-06-13 16:46:06 -0400

    Trying new calendar

--- Discipleship/composeApp/src/commonMain/kotlin/App.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/App.kt
@@ -12,38 +12,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 // At some point soon we need to define navigation
 fun App() {
     MaterialTheme {
-        Scaffold(
-//            bottomBar = { // will refactor
-//                NavigationBar(
-//                    modifier = Modifier.navigationBarsPadding(), containerColor = primaryContainerLight,
-//                    contentColor = secondaryLight) {
-//                    NavigationBarItem(
-//                        selected = true,
-//                        onClick = {},
-//                        icon = {
-//                            Icon(imageVector = Icons.Rounded.Person, contentDescription = "Social")
-//                        },
-//                        colors = NavigationBarItemDefaults.colors(secondaryLight)
-//                    )
-//                    NavigationBarItem(
-//                        selected = true,
-//                        onClick = {},
-//                        icon = {
-//                            Icon(imageVector = Icons.Rounded.Home, contentDescription = "Social")
-//                        },
-//                        colors = NavigationBarItemDefaults.colors(secondaryLight)
-//                    )
-//                    NavigationBarItem(
-//                        selected = true,
-//                        onClick = {},
-//                        icon = {
-//                            Icon(imageVector = Icons.Rounded.DateRange, contentDescription = "Social")
-//                        },
-//                        colors = NavigationBarItemDefaults.colors(secondaryLight)
-//                    )
-//                }
-//            }
-        ) {
+        Scaffold{
             // All of our main screen composables can go here
             //WelcomeScreen()
             //LoginScreen()
