b4bfa62cbdce984f19cedf6ee6fe2d35b4550d71
JWard2041 <josh.wrd2@outlook.com>
2024-06-17 13:25:20 -0400

    Added basic navigation

--- Discipleship/composeApp/src/commonMain/kotlin/App.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,5 @@
 import androidx.compose.runtime.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import acct_creation.LoginScreen
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import discipleship.WelcomeScreen
