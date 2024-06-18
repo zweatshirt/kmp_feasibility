85904fb65a7007f5714c1abddaba2db530c39f1f
JWard2041 <josh.wrd2@outlook.com>
2024-06-18 13:52:40 -0400

    fixing problems from previous day to get navigation and profile back

--- Discipleship/composeApp/src/commonMain/kotlin/discipleship/WelcomeScreen.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/discipleship/WelcomeScreen.kt
@@ -4,8 +4,6 @@ import acct_creation.LoginScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
