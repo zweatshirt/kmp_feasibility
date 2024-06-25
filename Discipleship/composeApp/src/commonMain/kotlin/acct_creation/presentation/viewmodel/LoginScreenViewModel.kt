53c59d2544cfd020de24b1a4d5bd9da114d92996
JWard2041 <josh.wrd2@outlook.com>
2024-06-25 14:50:22 -0400

    Added Validation to Login Screen

--- Discipleship/composeApp/src/commonMain/kotlin/acct_creation/presentation/viewmodel/LoginScreenViewModel.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/acct_creation/presentation/viewmodel/LoginScreenViewModel.kt
@@ -22,15 +22,82 @@ class LoginScreenViewModel: ViewModel() {
     private val scope = viewModelScope
     var firebaseUser: FirebaseUser? = null
     var userSignedIn = false
+
     var email by mutableStateOf("")
         private set
     var password by mutableStateOf("")
         private set
+
+    var emailResult by mutableStateOf(ValidationResult(false, null))
+    var passwordResult by mutableStateOf(ValidationResult(false, null))
+
+
     fun updateEmail(input: String) {
         email = input
     }
     fun updatePassword(input: String) {
         password = input
+    }
+
+    // Validate Email
+    private val emailAddressRegex = Regex(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+
+    private fun validateEmail(): ValidationResult {
+        if(email.isBlank()) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Email can't be blank."
+            )
+        }
+        if(!email.matches(emailAddressRegex)) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Not a valid email."
+            )
+        }
+        return ValidationResult(
+            successful = true
+        )
+    }
+
+    // Validate Password
+    private fun validatePassword(): ValidationResult {
+        if(password.length < 8) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password must contain at least 8 characters."
+            )
+        }
+        val containsLettersAndDigits = password.any { it.isDigit() } && password.any { it.isLetter() }
+        if(!containsLettersAndDigits) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Password must contain one letter and number."
+            )
+        }
+        return ValidationResult(
+            successful = true
+        )
+    }
+
+    fun loginIsValid(): Boolean {
+        emailResult = validateEmail()
+        passwordResult = validatePassword()
+
+
+        if (emailResult.successful &&
+            passwordResult.successful) {
+            return true
+        }
+        return false
     }
 
     fun firebaseAuth(): FirebaseUser? {
