e4845c855409b9f5ee3aa85b52c89e89f53386b5
JWard2041 <josh.wrd2@outlook.com>
2024-06-27 16:50:10 -0400

    added User object in signup

--- Discipleship/composeApp/src/commonMain/kotlin/profile/domain/model/Disciple.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/profile/domain/model/Disciple.kt
@@ -3,9 +3,10 @@ package profile.domain.model
 import org.jetbrains.compose.resources.DrawableResource
 
 class Disciple(
+    uID: String,
     firstName: String,
     lastName: String,
     email: String,
     image: DrawableResource?,
     bio: String):
-    User(firstName, lastName, email, image, bio)
+    User(uID, firstName, lastName, email, image, bio)
