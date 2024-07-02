6588a736333dc5dc731121e2ae4e0bf70c47a47d
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2024-07-02 16:27:07 -0400

    Adding Daos, Realm objects, next is full implementation and integration. Trouble with app config.

--- Discipleship/composeApp/src/commonMain/kotlin/profile/domain/model/User.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/profile/domain/model/User.kt
@@ -1,6 +1,8 @@
 package profile.domain.model
 
 import androidx.compose.ui.graphics.ImageBitmap
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import org.jetbrains.compose.resources.DrawableResource
 import org.mongodb.kbson.ObjectId
@@ -14,11 +16,11 @@ open class User(
     val bio: String? = null
 )
 
-open class RealmUser : RealmObject {
-    var _id: ObjectId = ObjectId()
+open class UserEntity : RealmObject {
+    var id: String = ""
     var firstName: String = ""
     var lastName: String = ""
     var email: String = ""
-    var image: ImageBitmap? = null
+    var image: RealmList<Byte> = realmListOf()
     var bio: String? = null
 }
