139b7c69cd401b7013a0fa982c5ecfc4f4791b09
JWard2041 <josh.wrd2@outlook.com>
2024-07-05 16:14:11 -0400

    updated tool card look

--- Discipleship/composeApp/src/commonMain/kotlin/home/presentation/ui/composables/ToolsSection.kt
+++ Discipleship/composeApp/src/commonMain/kotlin/home/presentation/ui/composables/ToolsSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,9 +15,12 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import home.domain.model.Tool
@@ -54,7 +58,7 @@ fun ToolCard(tool: Tool) {
 
     Card(
         modifier = Modifier
-            .width(240.dp)
+            .width(300.dp)
             .height(175.dp)
             .padding(start = 16.dp, end = 8.dp, bottom = 16.dp)
             .background(primaryContainerLight)
@@ -65,11 +69,16 @@ fun ToolCard(tool: Tool) {
     ) {
         Column(modifier = Modifier
             .padding(16.dp)
+            .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
+
             Row(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .fillMaxWidth()
+                ,
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = tName,
@@ -79,15 +88,23 @@ fun ToolCard(tool: Tool) {
                 )
             }
 
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(4.dp))
 
-            if (tDescription != null) {
-                Text(
-                    text = tDescription,
-                    fontSize = 12.sp,
-                    color = primaryLight,
-                    fontWeight = FontWeight.Medium
-                )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (tDescription != null) {
+                    Text(
+                        text = tDescription,
+                        fontSize = 12.sp,
+                        color = primaryLight,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
             }
         }
     }
